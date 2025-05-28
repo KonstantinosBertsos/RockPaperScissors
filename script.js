3dfbb9e4b022950c5f386c2e9bd1e9087d3484fe
Konstantinos <konstantinosbertsos@gmail.com>
2025-05-28 13:47:28 +0200

    Add Functions to buttons with EventListeners, updated Ui

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-console.log("Hello World!");
+window.addEventListener("DOMContentLoaded",() => {
 
 let humanScore = 0;
 let computerScore = 0;
+let getHumanChoice;
 
 function getComputerChoice(){
     let a = Math.floor(Math.random() * 3);
@@ -15,65 +16,102 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let a = prompt("Input your choice")
-    return a
-}
-
-function playRound(humanChoice, computerChoice){
-
-     humanChoice = getHumanChoice();
-     computerChoice = getComputerChoice();
+function playingFive(humanChoice, computerChoice){
 
     
+    if (humanScore >= 5 || computerScore >= 5) {
+        return; // Game is over
+    }
 
+
+    humanChoice = getHumanChoice;
+    computerChoice = getComputerChoice();
+    let message;
+
+        
     if ( humanChoice == "scissors" && computerChoice == "rock"){
         computerScore ++
-        return"Rock beats scissors, Pc wins"
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        message = "Rock beats scissors, Pc wins";
+        divDisplay.textContent = message;
          
     } else if ( computerChoice == "scissors" && humanChoice =="rock") {
         humanScore ++ 
-        return"Rock beats scissors, User wins"
-        
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        message = "Rock beats scissors, User wins";
+        divDisplay.textContent = message;
 
     } else if ( humanChoice == "paper" && computerChoice =="rock")  {
         humanScore ++
-        return "Paper beats rock, User wins"
-        
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        message = "Paper beats rock, User wins";
+        divDisplay.textContent = message;
 
     } else if ( computerChoice == "paper" && humanChoice =="rock")  {
         computerScore ++ 
-        return "Paper beats rock, Pc wins" 
-        
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        message = "Paper beats rock, Pc wins" ;
+        divDisplay.textContent = message;
 
     } else if ( humanChoice == "scissors" && computerChoice == "paper"){
         humanScore ++ 
-        return "Scissors beat paper, User wins"
-        
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        message = "Scissors beat paper, User wins";
+        divDisplay.textContent = message;
 
-    } else if ( computerChoice == "scissors " && humanChoice == "paper"){
+    } else if ( computerChoice == "scissors" && humanChoice == "paper"){
         computerScore ++
-        return "Scissors beat paper, Pc wins"
-        
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        message = "Scissors beat paper, Pc wins";
+        divDisplay.textContent = message;
     } else {
-        return " Its a draw"
+        message = " Its a draw";
+        scoreDisplay.textContent = " User's Score : " + humanScore+ " Pc Score : " + computerScore;
+        divDisplay.textContent = message;
     }
-    
 
-    }
-    
-    function playGame (){
-        for ( let i = 0; i < 5; i++){
-            console.log(playRound());
-            console.log(" User :" + humanScore + " Pc :" + computerScore )
-        }
+    if (humanScore === 5 || computerScore === 5) {
 
-        if(humanScore > computerScore){
-            console.log("User won the battles!")
-        }else {
-            console.log("Pc won the battles!")
+        if (humanScore === 5) {
+            divDisplay.textContent = "GAME WON";
+        } else {
+            divDisplay.textContent = "GAME OVER, PC WON";
         }
         
     }
-    console.log(playGame());
+
+
+        
+} 
+const btn1Ref = document.querySelector("#btn1");
+const btn2Ref = document.querySelector("#btn2");
+const btn3Ref = document.querySelector("#btn3");
+
+const display = document.querySelector("#display");
+const divDisplay = document.createElement("div");
+const scoreDisplay = document.createElement("div");
+
+display.setAttribute("style", "height: 50px")
+display.appendChild(divDisplay);
+display.appendChild(scoreDisplay);
+
+
+
+btn1Ref.addEventListener("click", () => {
+getHumanChoice = "rock"
+playingFive();
+});
+
+btn2Ref.addEventListener("click", () => {
+getHumanChoice = "paper"
+playingFive();
+});
     
+btn3Ref.addEventListener("click", ()=>{
+getHumanChoice = "scissors"
+playingFive();
+});
+
+}); 
+
+
